dde258691ffacfd410d34e19193b921d22407740
shafi9155 <76615248+shafi9155@users.noreply.github.com>
2023-02-06 02:51:06 +0530

    adding user page

--- client/src/components/Singlecmpln.js
+++ client/src/components/Singlecmpln.js
@@ -3,13 +3,13 @@ import React from 'react'
 function Singlecmpln() {
   return (
     <div className=" p-5 border-2 m-5 rounded-2xl border-custom-grey cmpln ">
-        <a href="" className="no-underline hover:underline  inline-block text-5xl text-custom-blue   " >Title</a>
-        <span className="   rounded-full p-3 text-xl  border-2 border-custom-grey">Private</span>
-        <span className=' block'>Ticket No: </span>
-        <p className=" ">Complain Related: Hostel</p>
+        <a href="" className="no-underline hover:underline p-4 inline-block text-5xl text-custom-blue   " >Title</a>
+        <span className="   rounded-full p-4 text-xl  float-right border-2 border-custom-grey">Private</span>
+        <span className='pl-4 block'>Ticket No: </span>
+        <p className="pl-4 ">Complain Related: Hostel</p>
 
-        <span className="inline-block text-sm">Date at Time</span>
-        <span className="float-right inline-block">Status:Accepted </span>
+        <span className="pl-4 inline-block text-sm">Date at Time</span>
+        <span className="float-right inline-block">Status: Accepted </span>
         
     </div>
   )
--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,18 +1,40 @@
 import React from 'react'
+import { useState } from 'react'
 import Navbar from '../components/Navbar'
 import Singlecmpln from '../components/Singlecmpln'
-function userProfile() {
+
+function UserProfile() {
+  const [showing,setshowing]=useState(false);
+  const [name,setname]=useState("Shafi");
   return (
-    <div>
+  
+      <div className=''>
   <Navbar/>
   <div className="parent  m-auto mt-20 flex flex-row">
-    <div className="bio basis-1/3">
+    <div className= " m-5 bio basis-1/3">
        <img src="/avatar.png" className="w-40 m-3"   alt=" profile img"/>
        <p className='p-1'>Name : Mohammad Shafiuddin</p>
        <p className='p-1'>Enrollment No: LCS2020053</p>
        <p className='p-1'>Email : shafiuddin9155@gmail.com</p>
        <p className='p-1'>Mobile No:9572326138</p>
-         <button className="bg-custom-blue text-white text-sm  p-2 rounded-full">Edit Profile</button>
+         <button className="bg-custom-blue edt-btn mt-5 text-white text-sm  p-2 rounded-lg" onClick={() => setshowing(true)}>Edit Profile</button>
+       <div className={showing?"block":"hidden"} >
+            <label for="">
+                <span className='mt-2 mb-1 inline-block'>Name</span>
+                <input className='border-2 border-slate-400 w-full p-1 rounded-lg' type="text"/>
+            </label>
+            <label for="">
+                <span className='mt-2 mb-1 inline-block'>Mobile No</span>
+                <input className='border-2 border-slate-400 w-full p-1 rounded-lg' type="text"/>
+            </label>
+            <label for="">
+                <span className='mt-2 mb-1 inline-block'>Change Photo</span>
+                <input className='border-2 mb-4 border-slate-400 w-full p-1 rounded-lg' type="file"/>
+            </label>
+           <button className='bg-custom-blue text-white text-sm  m-2 p-3 rounded-lg'>Save</button>
+           <button onClick={() => setshowing(false)} className='bg-custom-grey text-black text-sm  m-2 p-3 rounded-lg'>Cancel</button>
+         </div>
+         
     </div>
     <div className='basis-2/3'>
     <Singlecmpln/>
@@ -25,4 +47,4 @@ function userProfile() {
   )
 }
 
-export default userProfile
+export default UserProfile
